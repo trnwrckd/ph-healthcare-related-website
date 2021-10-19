c2571616461ea7511cf06d861635ad945ddbeb20
Ishmamur Rahman <trnwrckd@gmail.com>
2021-10-19 08:13:40 +0600

    fixed responsitve issues

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -9,38 +9,38 @@ const Header = () => {
         <div className="custom-nav fixed-top">
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid px-3">
+                    
                     <a className="navbar-brand" href="https://react-firebase-authentic-c6866.web.app/">
                         <img src="./images/navlogo.webp" alt="" width="30" height="30"></img>
                         <span className="logo-text">CovidSupport</span>
                     </a>
 
-                    <div className=" d-flex flex-column align-items-center">
-                        <div>
-                            <button className="navbar-toggler mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                        </div>
+                    <button className="navbar-toggler mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                        <div className="collapse navbar-collapse " id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className = "nav-item">
-                                    <NavLink to="/home"  className="common" activeClassName="active">Home</NavLink>
-                                </li>
-                                <li className = "nav-item">
-                                <NavLink to="/shop"  className="common" activeClassName="active">Shop</NavLink>
-                                </li>
-                                <li className = "nav-item">
-                                <NavLink to="/contact"  className="common" activeClassName="active">Contact</NavLink>
-                                </li>
-                                <li className = "nav-item">
-                                <NavLink to="/login"  className="common" activeClassName="active">Login</NavLink>
-                                </li>
-                                <li className = "nav-item">
-                                    {!isLoading && <span className="fs-6 me-2">{user.displayName}</span>}
-                                </li>
-                                <li className = "nav-item">{user.email && <button className='btn btn-danger' onClick={logOut}>Logout</button>}</li>
-                            </ul>
-                        </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className = "nav-item">
+                                <NavLink to="/home"  className="common" activeClassName="active">Home</NavLink>
+                            </li>
+                            <li className = "nav-item">
+                            <NavLink to="/shop"  className="common" activeClassName="active">Shop</NavLink>
+                            </li>
+                            <li className = "nav-item">
+                            <NavLink to="/contact"  className="common" activeClassName="active">Contact</NavLink>
+                            </li>
+                            <li className = "nav-item">
+                                {!isLoading && <span className="fs-6 me-2">{user.displayName}</span>}
+                            </li>
+                            <li className = "nav-item">
+                            {
+                                user.email ?
+                                    <button className='btn btn-danger' onClick={logOut}>Logout</button>
+                                : <NavLink to="/login" className="common" activeClassName="active">Login</NavLink>
+                            }
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </nav>
